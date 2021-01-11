afc81fb9f70a4dc71062d0ad343ed98d2faede38
Sebastian gomez a <sebas970218@gmail.com>
2021-01-11 13:37:42 -0500

    update javascript

--- simulador.js
+++ simulador.js
@@ -3,15 +3,27 @@ let VCProductor = 3;
 let VCConsumidor = 0;
 
 function addProduct() {
+    document.querySelector('#buttonConsumer').disabled = false;
     items = items + 1;
     VCProductor = VCProductor - 1;
     VCConsumidor = VCConsumidor + 1;
     document.querySelector('#items').innerText = items;
+    document.querySelector('#VCC').innerText = VCConsumidor;
+    document.querySelector('#VCP').innerText = VCProductor;
+    if (VCProductor == 0) {
+        document.querySelector('#buttonProducer').disabled = true;
+    }
 }
 
 function takeProduct() {
+    document.querySelector('#buttonProducer').disabled = false;
     items = items - 1;
     VCProductor = VCProductor + 1;
     VCConsumidor = VCConsumidor - 1;
     document.querySelector('#items').innerText = items;
+    document.querySelector('#VCC').innerText = VCConsumidor;
+    document.querySelector('#VCP').innerText = VCProductor;
+    if (VCConsumidor == 0) {
+        document.querySelector('#buttonConsumer').disabled = true;
+    }
 }
