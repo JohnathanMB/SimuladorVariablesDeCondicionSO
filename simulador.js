fce2375d1c88f8f559935c1ef4915a7a76378467
Johnathan Monsalve <desarrollo.gi1@udea.edu.co>
2021-01-11 16:03:08 -0500

    ajuste de colores en tabla

--- simulador.js
+++ simulador.js
@@ -16,16 +16,18 @@ function addProduct() {
     document.querySelector('#divCounter').style.backgroundColor = "#188E00";
     setTimeout(function () { document.querySelector('#divCounter').style.backgroundColor = null; }, 100);
 
-
+    if(items<4){
+        document.querySelector('#col'+items).style.backgroundColor = "#188E00";
+    }
 
 }
 
 function takeProduct() {
     document.querySelector('#buttonProducer').disabled = false;
-    items = items - 1;
+    
     VCProductor = VCProductor + 1;
     VCConsumidor = VCConsumidor - 1;
-    document.querySelector('#items').innerText = items;
+    
     document.querySelector('#VCC').innerText = VCConsumidor;
     document.querySelector('#VCP').innerText = VCProductor;
     if (VCConsumidor == 0) {
@@ -33,4 +35,11 @@ function takeProduct() {
     }
     document.querySelector('#divCounter').style.backgroundColor = "#26E600";
     setTimeout(function () { document.querySelector('#divCounter').style.backgroundColor = null; }, 100);
+
+    if(items>0){
+        document.querySelector('#col'+items).style.backgroundColor = "#26E600";
+    }
+    
+    items = items - 1;
+    document.querySelector('#items').innerText = items;
 }
